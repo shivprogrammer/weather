4eba50bc676a29aa14f650fde460bcfc660086f7
shivprogrammer <shiv.unadkat@gmail.com>
2021-08-03 09:20:10 -0400

    Successfully set up Redux store

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
